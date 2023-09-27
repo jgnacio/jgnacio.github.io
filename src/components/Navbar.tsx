9d41ead98dd5e156fe921331fd1b814addab574d
jgnacio <96546108+jgnacio@users.noreply.github.com>
2023-09-27 08:18:18 -0300

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,6 @@ export default function Navbar() {
   return (
     <div className="flex justify-between items-center fixed w-full top-0 z-40 p-4 md:px-10">
       <span className="normal-case text-lg font-thin">{"<jgnacio/>"}</span>
-      <Link href="/JGITechSolutions">
-        <span className="font-thin">JGITech Solutions</span>
-      </Link>
       <ThemeToggle />
     </div>
   );
